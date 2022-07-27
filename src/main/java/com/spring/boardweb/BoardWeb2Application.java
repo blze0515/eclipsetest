b863de34e34fbace542f670fc01d7671890a9ca4
blze0515 <blze0515@naver.com>
2022-07-27 15:37:06 +0900

    first eclipse commit

---
+++ src/main/java/com/spring/boardweb/BoardWeb2Application.java
@@ -0,0 +1,13 @@
+package com.spring.boardweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardWeb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardWeb2Application.class, args);
+	}
+
+}
